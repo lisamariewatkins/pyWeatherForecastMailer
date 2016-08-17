77614fda5d05a185603237ec0e13f39291798229
lisamariewatkins <lisamwatkins@ymail.com>
2016-08-17 13:05:16 -0500

    added email functionality

--- emailer.py
+++ emailer.py
@@ -1,4 +1,5 @@
 import requests
+import smtplib
 
 def convertFK(tempK):
         return int((tempK - 273.15) * 1.80 + 32)
@@ -23,7 +24,30 @@ def get_weather():
         forecast += '.'
         
 
-        print(forecast)
+        return forecast
+
+def send_emails(emails, schedule, forecast):
+        # connect to the smtp server
+        server = smtplib.SMTP('smtp.gmail.com', '587')
+        #start encryption
+        server.starttls()
+        #login
+        from_email = ''
+        username = input('What is your username?')
+        password = input("whats your password?")
+        server.login(username, password)
+
+        #Send to email list
+        for to_email, name in emails.items():
+                message = 'Subject: Today'
+                message += 'Hello, ' + name + '!' + '\n\n'
+                message += forecast + '\n\n'
+                message += schedule + '\n\n'
+                message += 'Have a great day!'
+                server.sendmail(from_email, to_email, message)
+                
+        server.quit()
+        
 
 def get_emails():
         emails = {}
@@ -54,6 +78,8 @@ def main():
         print (emails)
         schedule = get_schedule()
         print (schedule)
-        get_weather()
+        forecast = get_weather()
+        print(forecast)
+        send_emails(emails, schedule, forecast)
 
 main()
